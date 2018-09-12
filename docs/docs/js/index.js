fd276b06d6e0aa886f51ec9567377210963ab2f1
Angela Velasquez <angela.velasquez@laureate.net>
2018-09-12 08:26:34 -0600

    Removing unnecessary folder

--- docs/docs/js/index.js
+++
@@ -1,5 +0,0 @@
-
-if(window.location.pathname === '/lionware/silverbird') {
-    document.querySelectorAll('.aqua-gem').forEach( el => el.classList.add('hidden') );
-    document.querySelectorAll('.silverbird').forEach( el => el.classList.remove('hidden') );
-};
