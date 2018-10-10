92cab298ae3d14343858d910bf0cebef198132b6
Angela Velasquez <angela.velasquez@laureate.net>
2018-10-10 08:06:31 -0600

    Updating Forklift team members

--- js/index.js
+++ js/index.js
@@ -1,18 +1,18 @@
 
 if(window.location.pathname === '/silverbird') {
     document.querySelectorAll('.aqua-gem').forEach( el => el.classList.add('hidden') );
-    document.querySelectorAll('.wp').forEach( el => el.classList.remove('hidden') );
+    document.querySelectorAll('.forklift').forEach( el => el.classList.remove('hidden') );
     document.querySelectorAll('.silverbird').forEach( el => el.classList.remove('hidden') );
 };
 
 if(window.location.pathname === '/aquagem') {
-    document.querySelectorAll('.wp').forEach( el => el.classList.add('hidden') );
+    document.querySelectorAll('.forklift').forEach( el => el.classList.add('hidden') );
     document.querySelectorAll('.silverbird').forEach( el => el.classList.add('hidden') );
     document.querySelectorAll('.aqua-gem').forEach( el => el.classList.remove('hidden') );
 };
 
-if(window.location.pathname === '/warehousingpro') {
+if(window.location.pathname === '/forklift') {
     document.querySelectorAll('.aqua-gem').forEach( el => el.classList.add('hidden') );
     document.querySelectorAll('.silverbird').forEach( el => el.classList.add('hidden') );
-    document.querySelectorAll('.wp').forEach( el => el.classList.remove('hidden') );
+    document.querySelectorAll('.forklift').forEach( el => el.classList.remove('hidden') );
 };
