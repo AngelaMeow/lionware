2fede284ca2d959f064074f3506ac69373f386e1
Angela Velasquez <hello@angelato.design>
2018-11-14 15:18:31 -0600

    Added wallstreet page and team members updates

--- js/index.js
+++ js/index.js
@@ -1,18 +1,28 @@
 
 if(window.location.pathname === '/silverbird') {
     document.querySelectorAll('.aqua-gem').forEach( el => el.classList.add('hidden') );
-    document.querySelectorAll('.forklift').forEach( el => el.classList.remove('hidden') );
+    document.querySelectorAll('.forklift').forEach( el => el.classList.add('hidden') );
+    document.querySelectorAll('.wallstreet').forEach( el => el.classList.add('hidden') );
     document.querySelectorAll('.silverbird').forEach( el => el.classList.remove('hidden') );
 };
 
 if(window.location.pathname === '/aquagem') {
     document.querySelectorAll('.forklift').forEach( el => el.classList.add('hidden') );
     document.querySelectorAll('.silverbird').forEach( el => el.classList.add('hidden') );
+    document.querySelectorAll('.wallstreet').forEach( el => el.classList.add('hidden') );
     document.querySelectorAll('.aqua-gem').forEach( el => el.classList.remove('hidden') );
 };
 
 if(window.location.pathname === '/forklift') {
     document.querySelectorAll('.aqua-gem').forEach( el => el.classList.add('hidden') );
     document.querySelectorAll('.silverbird').forEach( el => el.classList.add('hidden') );
+    document.querySelectorAll('.wallstreet').forEach( el => el.classList.add('hidden') );
     document.querySelectorAll('.forklift').forEach( el => el.classList.remove('hidden') );
+};
+
+if(window.location.pathname === '/wallstreet') {
+    document.querySelectorAll('.aqua-gem').forEach( el => el.classList.add('hidden') );
+    document.querySelectorAll('.silverbird').forEach( el => el.classList.add('hidden') );
+    document.querySelectorAll('.forklift').forEach( el => el.classList.add('hidden') );
+    document.querySelectorAll('.wallstreet').forEach( el => el.classList.remove('hidden') );
 };
