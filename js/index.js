9fbea57d5758f2d9f9141e6b832dffbf2060bd4e
Angela Velasquez <hello@angelato.design>
2019-04-17 09:51:47 -0600

    Added new page for Scheneider project

--- js/index.js
+++ js/index.js
@@ -1,40 +1,5 @@
+var path = window.location.pathname;
+var pathelement = path.split('/').pop();
 
-if(window.location.pathname === '/silverbird') {
-    document.querySelectorAll('.aqua-gem').forEach( el => el.classList.add('hidden') );
-    document.querySelectorAll('.forklift').forEach( el => el.classList.add('hidden') );
-    document.querySelectorAll('.wallstreet').forEach( el => el.classList.add('hidden') );
-    document.querySelectorAll('.runway').forEach( el => el.classList.add('hidden') );
-    document.querySelectorAll('.silverbird').forEach( el => el.classList.remove('hidden') );
-};
-
-if(window.location.pathname === '/aquagem') {
-    document.querySelectorAll('.forklift').forEach( el => el.classList.add('hidden') );
-    document.querySelectorAll('.silverbird').forEach( el => el.classList.add('hidden') );
-    document.querySelectorAll('.wallstreet').forEach( el => el.classList.add('hidden') );
-    document.querySelectorAll('.runway').forEach( el => el.classList.add('hidden') );
-    document.querySelectorAll('.aqua-gem').forEach( el => el.classList.remove('hidden') );
-};
-
-if(window.location.pathname === '/forklift') {
-    document.querySelectorAll('.aqua-gem').forEach( el => el.classList.add('hidden') );
-    document.querySelectorAll('.silverbird').forEach( el => el.classList.add('hidden') );
-    document.querySelectorAll('.wallstreet').forEach( el => el.classList.add('hidden') );
-    document.querySelectorAll('.runway').forEach( el => el.classList.add('hidden') );
-    document.querySelectorAll('.forklift').forEach( el => el.classList.remove('hidden') );
-};
-
-if(window.location.pathname === '/wallstreet') {
-    document.querySelectorAll('.aqua-gem').forEach( el => el.classList.add('hidden') );
-    document.querySelectorAll('.silverbird').forEach( el => el.classList.add('hidden') );
-    document.querySelectorAll('.forklift').forEach( el => el.classList.add('hidden') );
-    document.querySelectorAll('.runway').forEach( el => el.classList.add('hidden') );
-    document.querySelectorAll('.wallstreet').forEach( el => el.classList.remove('hidden') );
-};
-
-if(window.location.pathname === '/runway') {
-    document.querySelectorAll('.aqua-gem').forEach( el => el.classList.add('hidden') );
-    document.querySelectorAll('.silverbird').forEach( el => el.classList.add('hidden') );
-    document.querySelectorAll('.forklift').forEach( el => el.classList.add('hidden') );
-    document.querySelectorAll('.wallstreet').forEach( el => el.classList.add('hidden') );
-    document.querySelectorAll('.runway').forEach( el => el.classList.remove('hidden') );
-};
+const cards = Array.from(document.querySelectorAll('.card')).filter( el => !el.classList.contains(pathelement) );
+cards.forEach(el => el.remove());
